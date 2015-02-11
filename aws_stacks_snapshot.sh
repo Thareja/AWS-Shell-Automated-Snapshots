30408a08c4fb0176d19021e8e61c3e1e52b80635
Thareja <Thareja@users.noreply.github.com>
2015-02-10 19:21:31 -0500

    Create aws_stacks_snapshot.sh

---
+++ aws_stacks_snapshot.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Filename    : aws_stacks_snapshot.sh
+# Created by  : Dhiraj Thareja - dhiraj_thareja@trimble.com
+# Version     : 1.1
+# Company     : AwesomeActually.com
+# Description : Creates snapshots and tag them for a list of stacks. 
+
+
+
+DATE=`date +'%m-%d-%Y-%H%M%S'`
+LOG_FILE=/var/log/stack_snapshots_$DATE.log #update the path for your choice
+FROM="your@email.com" #provide some random email id as from address
+TO="your@email.com" #Update the email id which you want to receive email alerts"
+REGION="us-east-1"
+STACKS=/opt/stack_names
+
+
+#StackName="LAMP stack-xxxxxx" #Enter Stack Name here. example: LAMP,JAVA
+cat $STACKS | while read StackName
+do
+COUNT=`ec2-describe-tags  --region $REGION  --filter "resource-type=instance" | grep "StackName" | grep "$StackName$" | awk '{ print $3 }' | sort -u | wc -l`
+ if [ $COUNT -eq 0 ]
+ then
+ echo "Stack : $StackName" | tee -a $LOG_FILE
+ echo "No isnatnces are found with stack Name $StackName" | tee -a $LOG_FILE
+ echo "Jump to next stack" | tee -a $LOG_FILE
+ echo "#########################"
+ else
+        echo "Stack : $StackName" | tee -a $LOG_FILE
+        echo "Found instances assosiated with stack $StackName" | tee -a $LOG_FILE
+        for i in ` ec2-describe-tags  --region $REGION  --filter "resource-type=instance" | grep "StackName" | grep "$StackName$" | awk '{ print $3 }' | sort -u`; do
+
+        echo "Describing the volumes attached to instance $i" | tee -a $LOG_FILE
+        VOLUME=`ec2-describe-volumes | grep $i | awk '{ print $2 }'`
+        echo $VOLUME | tee -a $LOG_FILE
+        sleep 1
+        echo "Describing the tags for the instance $i" | tee -a $LOG_FILE
+        ec2-describe-tags --region $REGION  --filter "resource-id=$i" | tee -a $LOG_FILE
+
+                for j in `ec2-describe-volumes | grep $i | awk '{ print $2 }'` ; do
+                DATE=`date +'%m-%d-%Y#%H:%M:%S'`
+                echo "Initiating snapshot for the volume $j" | tee -a $LOG_FILE
+                SNAP_DESC="($i)_${DATE}"
+                echo "Snap description $SNAP_DESC" | tee -a $LOG_FILE
+                ec2-create-snapshot --description $SNAP_DESC --region $REGION $j
+                        if [ $? != 0 ]
+                        then
+                        echo "Snaphot creation failed for volume $j" | tee -a $LOG_FILE
+                        #mail -s "Snaphot creation failed for volume $j" -r $FROM $TO
+                        else
+                        echo "Snapshot is created. Will be available in few mins." | tee -a $LOG_FILE
+                        echo   ".............." | tee -a $LOG_FILE
+                        sleep 5
+                                for tag in Name Owner StackName CustomerName
+                                do
+                                echo " " | tee -a $LOG_FILE
+                                echo "####Adding the tag "#$tag#"####" | tee -a $LOG_FILE
+                                ec2-describe-tags --region $REGION  --filter "resource-id=$i"  --filter "key=$tag" |  grep -o "$tag.*" | cut -f 2- | while read y
+                                do
+                                echo "Describing snap id" | tee -a $LOG_FILE
+                                SNAP_ID=`ec2-describe-snapshots | grep $j | grep "$SNAP_DESC" | awk '{ print $2 }'`
+                                echo "Snap ID: $SNAP_ID" | tee -a $LOG_FILE
+                                sleep 2
+                                ec2-create-tags --region $REGION $SNAP_ID --tag  $tag="$y"
+                                        if [ $? != 0 ]
+                                        then
+                                        echo "Updating tag($tag) failed for the snaphsot $SNAP_ID" | tee -a $LOG_FILE
+#Uncomment below mail command if you want to recive alerts. You will need to have Postfix and mailx installed in your server
+#                                       mail -s "Updating tag($tag) failed for the snaphsot $SNAP_ID" -r $FROM $TO
+                                        fi
+                                echo "Updated the tag($tag) for snapshot $SNAP_ID" | tee -a $LOG_FILE
+                                done
+                done
+          echo "#########################################################" | tee -a $LOG_FILE
+          echo "snapshot is created for volume $j and updated the tags" | tee -a $LOG_FILE
+          echo "#########################################################" | tee -a $LOG_FILE
+        fi
+        done
+ done
+fi
+echo "****Script Execution Completed*****"
+done
+done
